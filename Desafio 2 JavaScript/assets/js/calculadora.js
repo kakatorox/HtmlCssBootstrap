ed518aa402501b5d01a9eb81b8d5d41044b39350
kakatorox <felipetorresmora@outlook.es>
2022-03-02 18:06:32 -0300

    Creacion de Cachipun

--- Desafio 2 JavaScript/assets/js/calculadora.js
+++ Desafio 2 JavaScript/assets/js/calculadora.js
@@ -1,20 +1,10 @@
-function sumar() {
-    var num = parseInt(document.getElementById("idNum1").value) + parseInt(document.getElementById("idNum2").value);
+function cachi() {
+    var num = parseInt(document.getElementById("idNum1").value);
+
+    for (index = 1 ; index <=num ; index++) {
+        
+    alert(index);     
+    }
+    document.write();
     document.getElementById("idResultado").innerText = num;
-}
-function resta() {
-    var num = parseInt(document.getElementById("idNum1").value) - parseInt(document.getElementById("idNum2").value);
-    document.getElementById("idResultado").innerText = num;
-}
-function multiplicar() {
-    var num = parseInt(document.getElementById("idNum1").value) * parseInt(document.getElementById("idNum2").value);
-    document.getElementById("idResultado").innerText = num;
-}
-function dividir() {
-    var num = parseInt(document.getElementById("idNum1").value) / parseInt(document.getElementById("idNum2").value);
-    document.getElementById("idResultado").innerText = num;
-}
-function modulo() {
-    var num = parseInt(document.getElementById("idNum1").value) % parseInt(document.getElementById("idNum2").value);
-    document.getElementById("idResultado").innerText = num;
-}
+};
